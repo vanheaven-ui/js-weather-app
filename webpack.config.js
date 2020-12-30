79827a6d190075cb518a0bda2296af8515f342c2
vanheaven <vanheaven6@gmail.com>
2020-12-30 13:17:10 +0300

    Environment setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new Dotenv()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
