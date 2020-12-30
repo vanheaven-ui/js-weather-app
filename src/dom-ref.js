338ca10474d4d90084e7d23abe2a7c4dd2ef8421
vanheaven <vanheaven6@gmail.com>
2020-12-30 17:21:55 +0300

    Update dom reference file

---
+++ src/dom-ref.js
@@ -0,0 +1,6 @@
+const searchSect = document.getElementById('search-area');
+const displaySect = document.getElementById('display-area');
+const icon = document.querySelector('#display-area img');
+const userInput = document.getElementById('search');
+
+export { searchSect, displaySect, icon, userInput };
